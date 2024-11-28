0f4bf2215486c3f427700f6720761ca75d3e6f8d
Beyza <beyzatasci122@gmail.com>
2024-11-28 13:27:10 +0300

    initial commit

---
+++ components/Favorites.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="favorites">
+    <p>Favoriler</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Favorites",
+};
+</script>
+
+<style scoped>
+.favorites {
+  font-size: 16px;
+  color: #333;
+}
+</style>
